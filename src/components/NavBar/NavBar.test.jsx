97b7736df87fe59aff2e4e45b9e130a1ea097510
Edgar Dias <dias.s.edgar@gmail.com>
2023-02-23 19:38:56 +0000

    Creating base unit test files

---
+++ src/components/NavBar/NavBar.test.jsx
@@ -0,0 +1,9 @@
+import { it, describe } from 'vitest'
+
+describe('NavBar component', () => {
+
+    it('should work', (ctx) => {
+      // prints name of the test
+      console.log(ctx.meta.name)
+    })
+})
