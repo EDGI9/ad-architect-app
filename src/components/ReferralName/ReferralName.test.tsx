2bd9086ab29bc4a81977b2a55224f29f36c29a57
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-12 20:38:15 +0000

    Adding text verification to tests of referral components

--- src/components/ReferralName/ReferralName.test.tsx
+++ src/components/ReferralName/ReferralName.test.tsx
@@ -6,10 +6,10 @@ describe('ReferralName component', () => {
     let component: object;
     const testText: string = "Test String";
     it('Component works', () => {
-        const {getByTestId} = render(<ReferralName text={testText}/>);
+        const {getByTestId, getByText} = render(<ReferralName text={testText}/>);
         component = getByTestId('qa-referral-name');
         //@ts-ignore
         expect(component.classList.contains('c-referral-name'));
-        //Test to see if component renders text
+        expect(getByText(testText).textContent).toBe(testText)
     })
 });
--- src/components/ReferralQuote/ReferralQuote.test.tsx
+++ src/components/ReferralQuote/ReferralQuote.test.tsx
@@ -6,10 +6,10 @@ describe('ReferralQuote component', () => {
     let component: object;
     const testText: string = "Test String";
     it('Component works', () => {
-        const {getByTestId} = render(<ReferralQuote text={testText}/>);
+        const {getByTestId, getByText} = render(<ReferralQuote text={testText}/>);
         component = getByTestId('qa-referral-quote');
         //@ts-ignore
         expect(component.classList.contains('c-referral-quote'));
-        //Test to see if component renders text
+        expect(getByText(testText).textContent).toBe(testText)
     })
 });
