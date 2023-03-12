ec921302345fc17161db8449387b6ec8ab066afe
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-12 20:36:07 +0000

    Creating test for section bubble component

--- src/components/SectionBuble/SectionBubble.test.tsx
+++ src/components/SectionBuble/SectionBubble.test.tsx
@@ -1,8 +1,18 @@
-import {it, describe, beforeEach, afterEach, expect} from "vitest";
-import {fireEvent, render} from '@testing-library/react';
-// import {SectionBubble} from "./SectionBubble.js"
+import {it, describe, expect} from "vitest";
+import {render} from '@testing-library/react';
+import {SectionBubble} from "./SectionBubble.js"
 
 describe('SectionBubble component', () => {
-    it('Renders component', () => {
+    let component: object;
+    const testTitle: string = "Test Title";
+    const testText: string = "Test Text";
+
+    it('Component works', () => {
+        const {getByTestId, getByText} = render(<SectionBubble title={testTitle} text={testText}/>);
+        component = getByTestId('qa-section-bubble');
+        //@ts-ignore
+        expect(component.classList.contains('c-section-bubble'));
+        expect(getByText(testTitle).textContent).toBe(testTitle)
+        expect(getByText(testText).textContent).toBe(testText)
     })
 });
