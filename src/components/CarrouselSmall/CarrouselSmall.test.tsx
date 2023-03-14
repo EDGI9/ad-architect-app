c679c80fcdb9ecef48580103cbd4bf04e10ac136
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-14 19:26:19 +0000

    Creation of the small carrousel component

--- src/components/CarrouselSmall/CarrouselSmall.test.tsx
+++ src/components/CarrouselSmall/CarrouselSmall.test.tsx
@@ -1,8 +1,20 @@
-import {it, describe, beforeEach, afterEach, expect} from "vitest";
-import {fireEvent, render} from '@testing-library/react';
-// import {CarrouselSmall} from "./CarrouselSmall.js"
+import {it, describe, expect} from "vitest";
+import {render} from '@testing-library/react';
+import {CarrouselSmall} from "./CarrouselSmall.js"
+import { slider } from "../../__mock__/slider";
 
 describe('CarrouselSmall component', () => {
-    it('Renders component', () => {
+    let component: object;
+    const testImageSource: string = "testImage.jpg";
+
+    it.skip('Component works', () => {
+        const {getByTestId} = render(<CarrouselSmall slides={slider} />);
+        component = getByTestId('qa-carrousel-small');
+        //@ts-ignore
+        expect(component.classList.contains('c-carrousel-small'));
+        /* console.log(container);
+        expect(container).toContainElement('source')
+        expect(container).toContainElement('img') */
+        
     })
 });
