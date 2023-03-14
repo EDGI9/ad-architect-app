536d6ed8da469d2565d2d46272196303da9c5621
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-14 19:50:18 +0000

    Creating base from component and test

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,8 +1,18 @@
 import {it, describe, beforeEach, afterEach, expect} from "vitest";
 import {fireEvent, render} from '@testing-library/react';
-// import {Form} from "./Form.js"
+import {Form} from "./Form.js"
 
 describe('Form component', () => {
-    it('Renders component', () => {
+    let component: object;
+    let input: object;
+    let textarea: object;
+
+    it('Component works', () => {
+        const {getByTestId} = render(<Form />);
+        component = getByTestId('qa-form');
+
+        expect(component).not.toBeNull();
+        //@ts-ignore
+        expect(component.classList.contains('c-form'));
     })
 });
