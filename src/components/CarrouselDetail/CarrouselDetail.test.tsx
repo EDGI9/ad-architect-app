5b5e335c019a5102b73dac54282d36d52ad88108
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-14 19:24:39 +0000

    Creation of the carrousel details component

--- src/components/CarrouselDetail/CarrouselDetail.test.tsx
+++ src/components/CarrouselDetail/CarrouselDetail.test.tsx
@@ -1,8 +1,26 @@
-import {it, describe, beforeEach, afterEach, expect} from "vitest";
-import {fireEvent, render} from '@testing-library/react';
-// import {CarrouselDetail} from "./CarrouselDetail.js"
+import {it, describe, expect} from "vitest";
+import {render} from '@testing-library/react';
+import {CarrouselDetail} from "./CarrouselDetail.js"
+import { slider } from "../../__mock__/slider";
 
 describe('CarrouselDetail component', () => {
-    it('Renders component', () => {
+    let component: object;
+    /* Add Image interface */
+    const testImageSource: object = slider[0];
+    const testText: object = {title: "Test Title", description: "Test Description"}; // Add CarrouselDetail type globally
+
+    it('Component works', () => {
+        const {getByTestId, getByText} = render(<CarrouselDetail image={testImageSource} text={testText} />);
+        component = getByTestId('qa-carrousel-detail');
+        //@ts-ignore
+        expect(component.classList.contains('c-carrousel-detail'));
+        //@ts-ignore
+        expect(getByText(testText.title).textContent).toBe(testText.title)
+        //@ts-ignore
+        expect(getByText(testText.description).textContent).toBe(testText.description)
+        /* console.log(container);
+        expect(container).toContainElement('source')
+        expect(container).toContainElement('img') */
+        
     })
 });
