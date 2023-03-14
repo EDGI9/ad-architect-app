5427e985a55e1f8c31feb7c76b3effd8b01e46cf
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-14 19:50:59 +0000

    Creating base test for big carrousel and placeholder component

--- src/components/CarrouselBig/CarrouselBig.test.tsx
+++ src/components/CarrouselBig/CarrouselBig.test.tsx
@@ -1,8 +1,21 @@
-import {it, describe, beforeEach, afterEach, expect} from "vitest";
-import {fireEvent, render} from '@testing-library/react';
-// import {CarrouselBig} from "./CarrouselBig.js"
+import {it, describe, expect} from "vitest";
+import { render} from '@testing-library/react';
+import {CarrouselBig} from "./CarrouselBig.js"
+import { slider } from "../../__mock__/slider";
 
 describe('CarrouselBig component', () => {
-    it('Renders component', () => {
+    let component: object;
+
+    it.skip('Component works', () => {
+        const {getByTestId} = render(<CarrouselBig slides={slider} />);
+        component = getByTestId('qa-carrousel-big');
+        
+        expect(component).not.toBeNull();
+        //@ts-ignore
+        expect(component.classList.contains('c-carrousel-big'));
+        /* console.log(container);
+        expect(container).toContainElement('source')
+        expect(container).toContainElement('img') */
+        
     })
 });
---
+++ src/components/CarrouselBig/CarrouselBig.tsx
@@ -0,0 +1,5 @@
+export function CarrouselBig(props) {
+    return (
+        <div>carrousel placeholder</div>
+    )
+}
