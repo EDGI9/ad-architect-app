cf727b970959c0e10af58df912de7cd5a7dcff77
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-16 21:27:56 +0000

    Creation of Title story

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,3 +1,4 @@
+import "./Title.scss"
 interface Title {
     type: string,
     text: string
@@ -6,6 +7,7 @@ interface Title {
 export function Title(props: Title): JSX.Element {
     
     if (props.type && props.text) {
+        //@ts-ignore
         return <props.type data-testid="qa-title" className="c-title">{props.text}</props.type>
     } else {
         return <h1></h1>
