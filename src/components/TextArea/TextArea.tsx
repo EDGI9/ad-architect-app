8fb8dbd907cc6620e111f47e3bfe09b2e5274643
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-16 21:28:33 +0000

    Create TextArea story and styling

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,8 +1,25 @@
+import "./TextArea.scss"
+
 interface TextArea {
-    text: string
+    text?: string
+    type?: string
+    placeholder?: string
 }
+
 export function TextArea(props: TextArea): JSX.Element {
+    let textAreaType: string;
+    switch(props.type) {
+        case 'primary':
+            textAreaType = 'c-textarea--primary'
+            break
+        case 'secondary':
+            textAreaType = 'c-textarea--secondary'
+            break;
+        default:
+            textAreaType = 'c-textarea--primary'
+    }
+
     return (
-        <textarea data-testid="qa-textarea" className="c-textarea">{props.text}</textarea>
+        <textarea data-testid="qa-textarea" className={`c-textarea ${textAreaType}`} placeholder={props.placeholder}>{props.text}</textarea>
     )
 }
