b1b5a9df664379c9f2292967aaf78f845b472f5f
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-21 20:46:40 +0000

    Creation of the Referral Name component story and styling

--- src/components/ReferralName/ReferralName.tsx
+++ src/components/ReferralName/ReferralName.tsx
@@ -1,11 +1,26 @@
+import "./ReferralName.scss"
+import { Card } from "../Card/Card";
+import { Image } from "../Image/Image";
+import { Title } from "../Title/Title";
+import { images } from "../../__mock__/images";
+
 interface ReferralName {
-    text: string
+    title: string
+    subtitle: string
 }
 
 export function ReferralName(props: ReferralName): JSX.Element {
     return (
         <div data-testid="qa-referral-name" className="c-referral-name">
-            <p className="c-referral-name__text">{props.text}</p>
+            <Card borderRadius={6} backgroundColor="#ECAE85E5">
+                <div className="c-referral-name__image">
+                    <Image imageData={images[0]} height={70} width={70} />
+                </div>                
+                <div className="c-referral-name__text">
+                    <Title type="h2" text={props.title} />
+                    <p className="c-referral-name__subtitle">{props.subtitle}</p>
+                </div>                
+            </Card>
         </div>
     )
 }
