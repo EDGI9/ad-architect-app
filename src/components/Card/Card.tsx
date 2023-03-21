61ff1e45ec5fb35a1f0f292bb54d855d43f53e65
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-21 20:44:07 +0000

    Fixing some issues with the Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,14 @@
+import { CSSProperties, ReactNode } from "react"
 import "./Card.scss"
 interface Card {
     backgroundColor?: string
     borderRadius?: number
+    children: ReactNode
 }
 
-export function Card(props: Card) {
-    console.log(props);
-    
-    const style = {
+export function Card(props: Card): JSX.Element {
+
+    const style: CSSProperties = {
         backgroundColor: props.backgroundColor ? `${props.backgroundColor}` : "#FFF",
         borderRadius: props.borderRadius ? props.borderRadius : "auto"
     }
@@ -15,7 +16,6 @@ export function Card(props: Card) {
     return (
         <div data-testid="qa-card" className="c-card" style={style}>
             {
-            //@ts-ignore
             props.children
             }
         </div>
