7dc52bce02a6362e2b44db37a403aa1a939d0e8a
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-25 19:24:45 +0000

    Create the CarrouselDetails component styling and story

--- src/components/CarrouselDetail/CarrouselDetail.tsx
+++ src/components/CarrouselDetail/CarrouselDetail.tsx
@@ -1,26 +1,33 @@
+import { useState } from "react";
+import {Components} from "../../interfaces/components.d";
 import { Image } from "../Image/Image";
+import { Card } from "../Card/Card";
+import { Button } from "../Button/Button";
+import { Title } from "../Title/Title";
+import "./CarrouselDetail.scss"
 
-interface CarrouselDetail {
-    image: {
-        src: string,
-        name: string
-    }
-    text: {
-        title: string
-        description: string
-    }
-}
 
-export function CarrouselDetail(props: CarrouselDetail): JSX.Element {
+
+
+export function CarrouselDetail(props: Components.CarrouselDetail): JSX.Element {
+    let [detailsVisibility, setDetailsVisibility] = useState(false);
+    let visibiliTyClass:string = detailsVisibility === true ? 'c-carrousel-detail__details-card--visible':''
+    
+    function toggleDetailsContainer() {
+        setDetailsVisibility(previousValue => !previousValue);
+    }
+
     return (
         <div data-testid="qa-carrousel-detail" className="c-carrousel-detail">
-            <div>
-                <Image imageData={props.image} height={100} width={100}/>
-            </div>
-            <div>
-                <h3>{props.text.title}</h3>
-                <p>{props.text.description}</p>
-            </div>
+            <Image imageData={props.image} />
+
+            <Card backgroundColor="#E1E1E1" borderRadius={6} className={`c-carrousel-detail__details-card ${visibiliTyClass}`}>
+                <Button text="<" type="terceary" round={true} small={true} onClick={toggleDetailsContainer}></Button>
+                <div className="c-carrousel-detail__text-container">
+                    <Title type="h3" text={props.text.title}></Title>
+                    <p>{props.text.description}</p>
+                </div> 
+            </Card>
         </div>
     ) 
 }
