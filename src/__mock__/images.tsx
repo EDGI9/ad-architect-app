4b91a4c0b57808cd32813ab2435d0c7e2fb569b2
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-25 19:22:22 +0000

    Adding more images to project and mock

--- src/__mock__/images.tsx
+++ src/__mock__/images.tsx
@@ -1,11 +1,26 @@
 import Image from "../interfaces/Image.d";
 
 import Quotation from "../assets/svg/quotation.svg";
+import bigImg1 from "../assets/images/bigImg1.png";
+import bigImg2 from "../assets/images/bigImg2.png";
+import bigImg3 from "../assets/images/bigImg3.png";
 
 
 export const images: Image[] = [
     {
         src: Quotation,
         name: 'Quotation'
+    },
+    {
+        src: bigImg1,
+        name: 'Big Image 1'
+    },
+    {
+        src: bigImg2,
+        name: 'Big Image 2'
+    },
+    {
+        src: bigImg3,
+        name: 'Big Image 3'
     },
 ];
