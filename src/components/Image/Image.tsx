0c940a03856ac1e52fac77059da4c760dc4b878a
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-25 19:24:09 +0000

    Adding some fixes to IMage component

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,11 +1,11 @@
 import {Components} from "../../interfaces/components.d";
 
 export function Image(props: Components.Image): JSX.Element {
-    const width: string = `${props.width}px` || "100px";
-    const height: string = `${props.height}px` || "100px";
+    const width: string = props.width !== undefined ? `${props.width}px` : "auto";
+    const height: string = props.height !== undefined ? `${props.height}px` : "auto";
 
     return (
-        <picture data-testid="qa-image">
+        <picture data-testid="qa-image" className="c-image">
             <source src={props.imageData.src} height={height} width={width}/>
             <img src={props.imageData.src} alt={props.imageData.name} height={height} width={width}></img>
         </picture>
