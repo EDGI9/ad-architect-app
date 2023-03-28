4a3d431f91bdcd03e4ca408c6d7057226415ffc1
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-28 23:36:36 +0100

    Adding of more images and updating the images mockup

--- src/__mock__/images.tsx
+++ src/__mock__/images.tsx
@@ -1,11 +1,16 @@
 import { Images } from "../interfaces/Image.d";
 import quotation from "../assets/svg/quotation.svg";
 import search from "../assets/svg/search.svg";
+import accessability from "../assets/svg/accessability.svg";
+import twitter from "../assets/svg/twitter.svg";
+import instagram from "../assets/svg/instagram.svg";
 import bigImg1 from "../assets/images/bigImg1.png";
 import bigImg2 from "../assets/images/bigImg2.png";
 import bigImg3 from "../assets/images/bigImg3.png";
 import logo from "../assets/images/logo.png";
 import logoName from "../assets/images/logoName.png";
+import logo2 from "../assets/images/logo2.png";
+import logoName2 from "../assets/images/logoName2.png";
 
 
 export const images: Images.ImageList = {
@@ -16,6 +21,18 @@ export const images: Images.ImageList = {
     'icon_search' : {
         src: search,
         name: 'Search'
+    },
+    'icon_accessability' : {
+        src: accessability,
+        name: 'Accessability'
+    },
+    'icon_twitter' : {
+        src: twitter,
+        name: 'Twitter'
+    },
+    'icon_instagram' : {
+        src: instagram,
+        name: 'Instagram'
     },
     'big_img_1' : {
         src: bigImg1,
@@ -35,6 +52,14 @@ export const images: Images.ImageList = {
     },
     'logo_name' : {
         src: logoName,
+        name: 'Ad Architects'
+    },
+    'logo_2' : {
+        src: logo2,
+        name: 'Ad Architects Logo'
+    },
+    'logo_name_2' : {
+        src: logoName2,
         name: 'Ad Architects'
     },
 };
