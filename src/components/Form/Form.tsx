66f7e43fe3228373c1ecd83b817e0c07a414f00f
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-28 22:49:43 +0100

    Addding component styling and story for Form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,29 @@
-/* @ts-ignore */
+import {Components} from "../../interfaces/Components.d";
+import { Card } from "../Card/Card";
+import { TextInput } from '../TextInput/TextInput';
+import { TextArea } from '../TextArea/TextArea';
+import { Title } from '../Title/Title';
+import { Button } from '../Button/Button';
+import "./Form.scss"
+
+//@ts-ignore
 export function Form(props) {
     return (
-        <form data-testid="qa-form" className="c-form">
-            {props.children}
-        </form>
+        <Card backgroundColor="#FFF" borderRadius={5}>
+            <form data-testid="qa-form" className="c-form">
+                <header className="c-form__header">
+                    <Title type='h2' text='Let’s start a conversation! '/>
+                    <Title type='h5' text='Get in touch for any questions and ideas you may have and we’ll work together to find the best solution. '/>
+                </header>
+                <section className="c-form__main-section">
+                    <TextInput placeholder="Name" type="secondary"/> 
+                    <TextInput placeholder="Email" type="secondary"/> 
+                    <TextArea placeholder="Message" type="secondary"/>
+                </section>
+                <footer className="c-form__footer">
+                    <Button text='Send Message'/>
+                </footer>
+            </form>
+        </Card>
     )
 }
