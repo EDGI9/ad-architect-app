13dfc9b67c5bc432251a430ee1ed4c874315e380
Edgar Dias <dias.s.edgar@gmail.com>
2023-03-29 22:08:25 +0100

    Styling and structuring who we are page

--- src/views/WhoWeAre.tsx
+++ src/views/WhoWeAre.tsx
@@ -1,7 +1,37 @@
+import { Title } from "../components/Title/Title";
+import { EmployeeCard } from "../components/EmployeeCard/EmployeeCard";
+import { Button } from "../components/Button/Button";
+import { Card } from "../components/Card/Card";
+import { employees } from "../__mock__/employees";
+
 export function WhoWeAre(): JSX.Element{
+    let employeeList;
+
+    if (Object.keys(employees).length > 0) {
+        employeeList = Object.values(employees).map((employee, index) => {
+            return <EmployeeCard image={employee.image} title={employee.title}  subtitle={employee.subtitle} text={employee.text} backgroundColor="" key={index}></EmployeeCard>
+        })
+    }
 
     return (
-        <h1>WhoWeAre</h1>
+        <div className="max-w-screen-2xl">
+                <section className="flex justify-start my-16">
+                    <Title type="h1" text="Who we are"></Title>
+                </section>
+                <section className="flex justify-center my-28 ">
+                    <Card backgroundColor="#E1E1E1" className="w-screen flex justify-center items-center flex-col md:flex-row md:space-x-12">
+                        {employeeList}
+                    </Card>
+                </section>
+                <section className="flex justify-center my-28">
+                    <div className="flex justify-center items-start flex-col md:flex-row md:space-x-12 mx-16 md:mx-0">
+                        <div className="w-3/5">
+                            <Title type="h2" text="Let’s start a conversation!"></Title>
+                            <Title type="h6" text="Get in touch for any questions you may have and we’ll work together to find the best solution."></Title>
+                        </div>
+                        <Button text="get in touch"></Button>
+                    </div>
+                </section>
+        </div>
     )
-    
 }
