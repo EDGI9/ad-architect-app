a8f9cc0fa9fd88fe0632f2c07ba4421a433e7065
Edgar Dias <dias.s.edgar@gmail.com>
2023-04-02 22:50:03 +0100

    Update of slider mock fetch

--- src/components/EmployeeCard/EmployeeCard.stories.tsx
+++ src/components/EmployeeCard/EmployeeCard.stories.tsx
@@ -8,7 +8,7 @@ export default {
   title: 'Employee Card',
   component: EmployeeCard,
   args: {
-    image: slider[0],
+    image: slider.small_slider[0],
     title: "Test title",
     subtitle: "Test subtitle",
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
