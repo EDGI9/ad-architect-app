0ae1044175975b414f55d098d653a55471fad74b
Edgar Dias <dias.s.edgar@gmail.com>
2023-04-07 17:05:17 +0100

    Adding safeguards for the carrousel detail component and the texts

--- src/components/CarrouselDetail/CarrouselDetail.tsx
+++ src/components/CarrouselDetail/CarrouselDetail.tsx
@@ -11,7 +11,9 @@ import "./CarrouselDetail.scss"
 
 export function CarrouselDetail(props: Components.CarrouselDetail): JSX.Element {
     let [detailsVisibility, setDetailsVisibility] = useState(false);
-    let visibiliTyClass:string = detailsVisibility === true ? 'c-carrousel-detail__details-card--visible':''
+    let visibiliTyClass:string = detailsVisibility === true ? 'c-carrousel-detail__details-card--visible':'';
+    const title: string = props.text?.title !== undefined ? props.text.title : "Title";
+    const description: string = props.text?.description !== undefined ? props.text.description : "Title";
     
     function toggleDetailsContainer() {
         setDetailsVisibility(previousValue => !previousValue);
@@ -24,8 +26,8 @@ export function CarrouselDetail(props: Components.CarrouselDetail): JSX.Element 
             <Card backgroundColor="#E1E1E1" borderRadius={6} className={`c-carrousel-detail__details-card ${visibiliTyClass}`}>
                 <Button text="<" type="terceary" round={true} small={true} onClick={toggleDetailsContainer}></Button>
                 <div className="c-carrousel-detail__text-container">
-                    <Title type="h3" text={props.text.title}></Title>
-                    <p>{props.text.description}</p>
+                    <Title type="h3" text={title}></Title>
+                    <p>{description}</p>
                 </div> 
             </Card>
         </div>
