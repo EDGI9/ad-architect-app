0a8e271fb7b617c814ffc1be9ec1a7da50c6e087
Edgar Dias <dias.s.edgar@gmail.com>
2023-04-07 17:04:31 +0100

    Update of Image interface and  slider mock data

--- src/__mock__/slider.tsx
+++ src/__mock__/slider.tsx
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import {Images} from "../interfaces/Image.d";
 import bigSliderImg1 from "../assets/images/bigSliderImg1.png";
 import bigSliderImg2 from "../assets/images/bigSliderImg2.png";
@@ -11,38 +12,70 @@ import smallSliderImg4 from "../assets/images/smallSliderImg4.png";
 const bigImageSlider: Images.Image[] = [
     {
         src: bigSliderImg1,
-        name: 'bigSliderImg1'
+        name: 'bigSliderImg1',
+        text: {
+            title: faker.name.jobDescriptor(),
+            description: faker.lorem.lines( 1)
+        }
     },
     {
         src: bigSliderImg2,
-        name: 'bigSliderImg2'
+        name: 'bigSliderImg2',
+        text: {
+            title: faker.name.jobDescriptor(),
+            description: faker.lorem.lines( 1)
+        }
     },
     {
         src: bigSliderImg3,
-        name: 'bigSliderImg3'
+        name: 'bigSliderImg3',
+        text: {
+            title: faker.name.jobDescriptor(),
+            description: faker.lorem.lines( 1)
+        }
     },
     {
         src: bigSliderImg4,
-        name: 'bigSliderImg4'
+        name: 'bigSliderImg4',
+        text: {
+            title: faker.name.jobDescriptor(),
+            description: faker.lorem.lines( 1)
+        }
     }
 ]
 
 const smallImageSlider: Images.Image[] = [
     {
         src: smallSliderImg1,
-        name: 'smallSliderImg1'
+        name: 'smallSliderImg1',
+        text: {
+            title: faker.name.jobDescriptor(),
+            description: faker.lorem.lines( 1)
+        }
     },
     {
         src: smallSliderImg2,
-        name: 'smallSliderImg2'
+        name: 'smallSliderImg2',
+        text: {
+            title: faker.name.jobDescriptor(),
+            description: faker.lorem.lines( 1)
+        }
     },
     {
         src: smallSliderImg3,
-        name: 'smallSliderImg3'
+        name: 'smallSliderImg3',
+        text: {
+            title: faker.name.jobDescriptor(),
+            description: faker.lorem.lines( 1)
+        }
     },
     {
         src: smallSliderImg4,
-        name: 'smallSliderImg4'
+        name: 'smallSliderImg4',
+        text: {
+            title: faker.name.jobDescriptor(),
+            description: faker.lorem.lines( 1)
+        }
     }
 ]
 
--- src/interfaces/Image.d.tsx
+++ src/interfaces/Image.d.tsx
@@ -2,6 +2,10 @@ export namespace Images {
     export interface Image {
         src: string
         name: string
+        text: {
+            title: string
+            description: string
+        }
     }
     export interface ImageList {
         [key: string]: Image
