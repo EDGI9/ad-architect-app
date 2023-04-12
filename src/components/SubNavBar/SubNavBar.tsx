d0f6e793386cfb084771e12dccabf4fb2c854926
Edgar Dias <dias.s.edgar@gmail.com>
2023-04-12 16:42:49 +0000

    Adding of an active state to the subnavbar and a click event

--- src/components/SubNavBar/SubNavBar.tsx
+++ src/components/SubNavBar/SubNavBar.tsx
@@ -3,13 +3,19 @@ import { Button } from "../Button/Button";
 import "./SubNavBar.scss"
 
 export function SubNavBar(props: Components.SubNavBar): JSX.Element {
+    const checkCurrentActiveNav = (item: string) => {
+        if (props.currentNav == undefined || props.currentNav == null) {
+            return false
+        }
+        return props.currentNav == item
+    } 
     return (
         <nav data-testid="qa-sub-nav-bar" className="c-sub-nav-bar">
             <ul className="c-sub-nav-bar__list">
                 {
                     Object.values(props.items).map((itemTitle, index) => (
-                        <li title={itemTitle} key={index} className="c-sub-nav-bar__list-item">
-                            <Button text={itemTitle} type="secondary"></Button>
+                        <li title={itemTitle.text} key={index} className="c-sub-nav-bar__list-item">
+                            <Button key={index} text={itemTitle.text} type="secondary" active={checkCurrentActiveNav(itemTitle.text)} onClick={() => props.onClick(itemTitle.text)}></Button>
                         </li>
                     ))
                 } 
