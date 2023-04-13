be8ad22047a111f8fc8c04da720848aadbd44f86
Edgar Dias <dias.s.edgar@gmail.com>
2023-04-13 19:42:27 +0000

    Update of the subNavBar component and test

--- src/components/SubNavBar/SubNavBar.test.tsx
+++ src/components/SubNavBar/SubNavBar.test.tsx
@@ -1,35 +1,59 @@
 import {it, describe, expect, vi} from "vitest";
-import { fireEvent, render, cleanup } from '@testing-library/react';
+import { fireEvent, render, cleanup, RenderResult } from '@testing-library/react';
 import {SubNavBar} from "./SubNavBar.js"
 import {RenovationPaths} from "../../router/renovations-paths.js";
 import {Components} from "../../interfaces/Components.d";
+import { exec } from "@faker-js/faker/modules/helpers/unique.js";
 
 
 describe('SubNavBar component', () => {
-    let component: object;
-    const handleClick = vi.fn((): void => {})
     const props: Components.SubNavBar = {
         items: RenovationPaths,
-        onClick: handleClick,
     };
+    
 
     afterAll(() => {
         cleanup();
     });
 
-    it('Component works', async () => {
-        const {getByTestId, getByText, getAllByRole} = render(<SubNavBar {...props} />);
-        component = getByTestId('qa-sub-nav-bar');
+    it('It should render properly', async () => {
 
-        expect(component).not.toBeNull();
-        //@ts-ignore
-        expect(component.classList.contains('c-sub-nav-bar'));
-        Object.entries(props.items).map(([key, item]) => {
-            expect(getByText(item.text).textContent).toBe(props.items[key].text)
-        })
+        //think about the option of having items optional
+        const component = render(<SubNavBar items={RenovationPaths}/>);
 
-        //@ts-ignore
-        await fireEvent.click(getAllByRole('button')[0])
-        expect(handleClick).toHaveBeenCalledTimes(1);
-    })
+        const nav = component.getByTestId('qa-sub-nav-bar');
+        const navList = component.getByTestId('qa-sub-nav-bar__list');
+        const navListItem = component.getAllByTestId('qa-sub-nav-bar__list-item');
+        const navListItemButton = navListItem[0].children[0];
+               
+        expect(nav).toBeTruthy();
+        expect(navList).toBeTruthy();
+        expect(navListItem).toBeTruthy();
+        expect(navListItemButton).toBeTruthy();
+        expect(navList.children.length).toEqual(props.items.length);
+
+        component.unmount();
+    });
+
+    it.only('It should handle user events and emit the selected item information', async () => {
+
+        const firstItem = RenovationPaths[0];
+
+        function handleClick(houseArea: Components.SubNAvBarItem):void {
+            expect(houseArea.id).toEqual(firstItem.id);
+            expect(houseArea.text).toEqual(firstItem.text);
+        }
+        
+        const component = render(<SubNavBar items={RenovationPaths} onClick={handleClick}/>);      
+
+        const navListItem = component.getAllByTestId('qa-sub-nav-bar__list-item');
+        const navListItemButton = navListItem[0].children[0];
+       
+        expect(navListItem).toBeTruthy();
+        expect(navListItemButton).toBeTruthy();
+
+        fireEvent.click(navListItemButton);
+        
+        component.unmount();
+    });
 });
--- src/components/SubNavBar/SubNavBar.tsx
+++ src/components/SubNavBar/SubNavBar.tsx
@@ -1,21 +1,35 @@
+import { useEffect, useState } from "react";
 import {Components} from "../../interfaces/Components.d";
 import { Button } from "../Button/Button";
 import "./SubNavBar.scss"
 
 export function SubNavBar(props: Components.SubNavBar): JSX.Element {
-    const checkCurrentActiveNav = (item: string) => {
-        if (props.currentNav == undefined || props.currentNav == null) {
-            return false
+
+    const {items, activeItem, onClick} = props;
+    const [currentAtiveId, setCurrentAtiveId]  = useState('');
+
+    function itemClick(item: Components.SubNAvBarItem):void {
+        if(!onClick) {
+            return 
         }
-        return props.currentNav == item
-    } 
+        const clickedItem: Components.SubNAvBarItem = {id:item.id , text: item.text}
+        setCurrentAtiveId(item.id)
+        onClick(clickedItem)
+    }
+
+    useEffect(() => {
+        if (activeItem && activeItem.id) {
+            setCurrentAtiveId(activeItem.id) 
+        }
+    },[activeItem])
+    
     return (
         <nav data-testid="qa-sub-nav-bar" className="c-sub-nav-bar">
-            <ul className="c-sub-nav-bar__list">
+            <ul className="c-sub-nav-bar__list" data-testid="qa-sub-nav-bar__list">
                 {
-                    Object.values(props.items).map((itemTitle, index) => (
-                        <li title={itemTitle.text} key={index} className="c-sub-nav-bar__list-item">
-                            <Button key={index} text={itemTitle.text} type="secondary" active={checkCurrentActiveNav(itemTitle.text)} onClick={() => props.onClick(itemTitle.text)}></Button>
+                    items.map((item, index) => (
+                        <li title={item.text} key={index} className="c-sub-nav-bar__list-item" data-testid="qa-sub-nav-bar__list-item">
+                            <Button key={index} text={item.text} type="secondary" active={item.id === currentAtiveId ? true : false} onClick={() => itemClick(item)}></Button>
                         </li>
                     ))
                 } 
