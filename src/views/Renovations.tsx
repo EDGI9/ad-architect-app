a1f59531fba1cd9cc6bdf82a01b496d0f90a786b
Edgar Dias <dias.s.edgar@gmail.com>
2023-04-13 19:44:08 +0000

    Update of the renovations page suing the new slider mock

--- src/views/Renovations.tsx
+++ src/views/Renovations.tsx
@@ -1,5 +1,5 @@
-import { useState , useEffect} from 'react';
-import {Images} from "../interfaces/Image.d";
+import { useState } from 'react';
+import { Images } from "../interfaces/Image.d";
 import { PageTitle } from "../components/PageTitle/PageTitle";
 import { SubNavBar } from "../components/SubNavBar/SubNavBar";
 import { CarrouselBig } from "../components/CarrouselBig/CarrouselBig";
@@ -11,38 +11,39 @@ import { Counter } from "../components/Counter/Counter";
 import { Title } from "../components/Title/Title";
 import { slider } from "../__mock__/slider";
 import { RenovationPaths } from "../router/renovations-paths"
+import {Components} from "../interfaces/Components.d";
+
 
 export function Renovations(): JSX.Element {
-    const image: Images.Image = {id: 1 , src:'', name:'', text:{title:'', description:''}}// Figure out cleaner way of doing this
-
-    let [currentImage, setCurrentImage] = useState(image);
-    let [currentImageIndex, setCurrentImageIndex] = useState(1);
-    let [currentNav, setCurrentNav] = useState("");
+    let [currentImageIndex, setCurrentImageIndex] = useState(1); 
+    let [currentHouseArea, setCurrentHouseArea] = useState(RenovationPaths[3]);
+    let [currentImage, setCurrentImage] = useState(slider[currentHouseArea.id][0]);
 
     const updateCurrentImage = (image: Images.Image) => {
-        const index = slider.findIndex(item => item.name == image.name) + 1;
+        const index = slider[currentHouseArea.id].findIndex(item => item.name == image.name) + 1;
         setCurrentImageIndex(index);
         setCurrentImage(image);
     }
 
-    const updateCurrentNav = (navTitle:string):void => {
-        setCurrentNav(navTitle)
+    const updateCurrentHouseArea= (houseArea: Components.SubNAvBarItem):void => {
+        setCurrentHouseArea(houseArea);
     }
-
-    useEffect( ()=> {
-        setCurrentImage(slider[0]);
-        setCurrentNav(RenovationPaths.kitchens.text)
-    } , [])
-    
    
     return (
         <div>
             <section className="flex justify-start my-16 text-sm">
                 <PageTitle text="Renovations"/>
             </section>
-            <SubNavBar items={RenovationPaths} currentNav={currentNav} onClick={updateCurrentNav}></SubNavBar>
+            <SubNavBar 
+                items={RenovationPaths} 
+                activeItem={currentHouseArea}
+                onClick={updateCurrentHouseArea} 
+            />
+
             <section className="mt-28">
-                <CarrouselBig slides={slider}></CarrouselBig>
+                <CarrouselBig 
+                    slides={slider[currentHouseArea.id]}
+                />
             </section>
             <Card backgroundColor='#E1E1E1' className='md:!bg-white'>
                 <section className="flex justify-center flex-col items-center">
@@ -51,19 +52,35 @@ export function Renovations(): JSX.Element {
                         <div className='flex justify-self-start self-start flex-col md:flex-row md:items-center mb-4'>
                             <Counter number={currentImageIndex} type="secondary" className="mr-5"/>
                             <div className='flex items-center gap-2'>
-                                <Title type='h3' text={currentNav} className='text-[#7B7B7B]' /> / <Title type='h3' text={currentImage.text.title} className='text-[#7B7B7B] font-light' />
+                                <Title 
+                                    type='h3' 
+                                    text={currentHouseArea.text} 
+                                    className='text-[#7B7B7B]' 
+                                /> 
+                                / 
+                                <Title 
+                                    type='h3' 
+                                    text={currentImage.text.title} 
+                                    className='text-[#7B7B7B] font-light' 
+                                />
                             </div>
                         </div>
                         <div>
-                            <CarrouselDetail image={currentImage} text={currentImage.text}></CarrouselDetail>
+                            <CarrouselDetail 
+                                image={currentImage} 
+                                text={currentImage.text} 
+                            />
                         </div>
                     </div>
                     <div className='w-screen sm:w-full lg:w-2/3 mt-5'>
-                        <CarrouselSmall slides={slider} onClick={updateCurrentImage}></CarrouselSmall>
+                        <CarrouselSmall 
+                            slides={slider[currentHouseArea.id]} 
+                            onClick={updateCurrentImage} 
+                        />
                     </div>
                 </section>
              </Card>
-            <section className="flex justify-center mt-28">
+            <section className="flex justify-center mt-10">
                 <ContactBlock title='Let’s start a conversation!' text='Get in touch for any questions you may have and we’ll work together to find the best solution.'/>
             </section>
         </div>
