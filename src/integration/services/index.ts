2306f63b1754747f89b64e80cba0928365f9d55a
Edgar Dias <dias.s.edgar@gmail.com>
2023-06-12 20:46:14 +0100

    Creation of the New Builds page and creation of a house service to fetch house areas

--- src/integration/services/index.ts
+++ src/integration/services/index.ts
@@ -1,9 +1,12 @@
 import { ImagesService } from "./Images.service";
 import { SliderService } from "./Slider.service";
+import { HouseService } from "./House.service";
 import { ImagesReaderAdapter } from "../adapters/ImagesReader.adapter";
 import { SliderImagesReaderAdapter } from "../adapters/SliderImages.adapter";
+import { HouseReaderAdapter } from "../adapters/HouseReader.adapter";
 
 export const Services = {
     Images: ImagesService(ImagesReaderAdapter()),
-    Slider: SliderService(SliderImagesReaderAdapter())
+    Slider: SliderService(SliderImagesReaderAdapter()),
+    HouseAreas: HouseService(HouseReaderAdapter())
 } as const;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import {RouterPaths} from "./RouterPaths";
 import {Home} from "../views/Home";
 import {Renovations} from "../views/Renovations";
+import {NewBuilds} from "../views/NewBuilds";
 import {WhoWeAre} from "../views/WhoWeAre";
 import {ContactUs} from "../views/ContactUs";
 
@@ -10,6 +11,7 @@ export function Router(): JSX.Element {
         <Routes>
             <Route path={RouterPaths.HOME.path} element={<Home />}/>
             <Route path={RouterPaths.RENOVATIONS.path} element={<Renovations />}/>
+            <Route path={RouterPaths.NEW_BUILDS.path} element={<NewBuilds />}/>
             <Route path={RouterPaths.WHO_WE_ARE.path} element={<WhoWeAre />}/>
             <Route path={RouterPaths.CONTACT_US.path} element={<ContactUs />}/>
         </Routes>
